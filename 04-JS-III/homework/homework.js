4ef83b9ebbf6cb90887353bc552d828d3c28980d
Manuel <manuelguaicara17@gmail.com>
2022-08-10 14:21:49 -0300

    tarea

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -171,7 +171,12 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-
+  var primero = n.toString()
+  if (primero[0] === '9') {
+    return true
+  } else {
+    return false
+  }
 }
 
 
@@ -179,7 +184,13 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-
+  var palabra = arreglo[0]
+    for (let i = 1; i < arreglo.length; i++) {
+        if (arreglo[i] !== palabra) {
+            return false
+        } 
+    } 
+    return true
 }
 
 
